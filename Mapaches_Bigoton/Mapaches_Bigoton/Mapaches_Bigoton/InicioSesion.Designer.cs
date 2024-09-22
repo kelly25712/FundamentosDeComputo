e45c96812e4ea93b09f2cc164571da85a42a0468
jimenabh02 <L20TE0232@teziutlan.tecnm.mx>
2024-09-22 16:17:19 -0600

    inicioSesion
    ajuste de color y tamaño

--- Mapaches_Bigoton/Mapaches_Bigoton/Mapaches_Bigoton/InicioSesion.Designer.cs
+++ Mapaches_Bigoton/Mapaches_Bigoton/Mapaches_Bigoton/InicioSesion.Designer.cs
@@ -40,10 +40,12 @@
             // 
             // pictureBox1
             // 
+            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
             pictureBox1.Image = Properties.Resources._3499102;
-            pictureBox1.Location = new Point(300, 12);
+            pictureBox1.Location = new Point(53, 11);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(198, 169);
+            pictureBox1.Size = new Size(133, 126);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
@@ -51,26 +53,30 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(373, 201);
+            label1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            label1.Location = new Point(96, 151);
             label1.Name = "label1";
-            label1.Size = new Size(59, 20);
+            label1.Size = new Size(55, 17);
             label1.TabIndex = 1;
             label1.Text = "Usuario";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(361, 265);
+            label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            label2.Location = new Point(85, 192);
             label2.Name = "label2";
-            label2.Size = new Size(83, 20);
+            label2.Size = new Size(77, 17);
             label2.TabIndex = 2;
             label2.Text = "Contraseña";
             // 
             // btnIniciarSesion
             // 
-            btnIniciarSesion.Location = new Point(169, 344);
+            btnIniciarSesion.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            btnIniciarSesion.Location = new Point(12, 255);
+            btnIniciarSesion.Margin = new Padding(3, 2, 3, 2);
             btnIniciarSesion.Name = "btnIniciarSesion";
-            btnIniciarSesion.Size = new Size(124, 29);
+            btnIniciarSesion.Size = new Size(108, 22);
             btnIniciarSesion.TabIndex = 3;
             btnIniciarSesion.Text = "Iniciar Sesion";
             btnIniciarSesion.UseVisualStyleBackColor = true;
@@ -78,9 +84,11 @@
             // 
             // btnCancelar
             // 
-            btnCancelar.Location = new Point(505, 344);
+            btnCancelar.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            btnCancelar.Location = new Point(144, 255);
+            btnCancelar.Margin = new Padding(3, 2, 3, 2);
             btnCancelar.Name = "btnCancelar";
-            btnCancelar.Size = new Size(94, 29);
+            btnCancelar.Size = new Size(82, 22);
             btnCancelar.TabIndex = 4;
             btnCancelar.Text = "Cancelar";
             btnCancelar.UseVisualStyleBackColor = true;
@@ -88,24 +96,27 @@
             // 
             // txtUsuario
             // 
-            txtUsuario.Location = new Point(336, 235);
+            txtUsuario.Location = new Point(64, 168);
+            txtUsuario.Margin = new Padding(3, 2, 3, 2);
             txtUsuario.Name = "txtUsuario";
-            txtUsuario.Size = new Size(125, 27);
+            txtUsuario.Size = new Size(110, 23);
             txtUsuario.TabIndex = 5;
             // 
             // txtContraseña
             // 
-            txtContraseña.Location = new Point(336, 288);
+            txtContraseña.Location = new Point(64, 208);
+            txtContraseña.Margin = new Padding(3, 2, 3, 2);
             txtContraseña.Name = "txtContraseña";
             txtContraseña.PasswordChar = '*';
-            txtContraseña.Size = new Size(125, 27);
+            txtContraseña.Size = new Size(110, 23);
             txtContraseña.TabIndex = 6;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.Moccasin;
+            ClientSize = new Size(239, 287);
             Controls.Add(txtContraseña);
             Controls.Add(txtUsuario);
             Controls.Add(btnCancelar);
@@ -113,6 +124,7 @@
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(pictureBox1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
